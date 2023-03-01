14bf61fcf7d963713befdfc2da4a1c72e4d6cbd3
Tautvydas Žukauskas <118177903+Zukauskas@users.noreply.github.com>
2023-03-01 19:03:52 +0200

    localStorage

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,24 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import AccountList from './Components/AccountList';
 import AccountSummary from './Components/AccountSummary';
 import AddNewAccount from './Components/AddNewAccount';
 
 function App() {
-    const [account, setAccount] = useState([]);
+    const [account, setAccount] = useState(
+        JSON.parse(localStorage.getItem('accounts')) || []
+    );
 
     const accountHandler = (name, lastName) => {
-        console.log(name, lastName);
         setAccount((prevState) => [
             ...prevState,
             { name, lastName, id: Math.random(), sum: 0, enteredAmount: '' },
         ]);
     };
+
+    useEffect(() => {
+        localStorage.setItem('accounts', JSON.stringify(account));
+    }, [account]);
 
     return (
         <div className="App">
