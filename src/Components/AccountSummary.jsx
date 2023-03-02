69b21b164310476a817d41ff299af70c3f5379ba
Tautvydas Žukauskas <118177903+Zukauskas@users.noreply.github.com>
2023-03-02 16:46:44 +0200

    style summary

--- src/Components/AccountSummary.jsx
+++ src/Components/AccountSummary.jsx
@@ -5,12 +5,24 @@ const AccountSummary = ({ accounts }) => {
     );
 
     return (
-        <div className='mb-4'>
-            <p className='text-gray-700 font-bold'>
-                Total accounts:
-                {accounts.length}
-            </p>
-            <p class='text-gray-700 font-bold'>Total money: {totalMoney}</p>
+        <div className='mb-8 flex items-center'>
+            <h2 className='text-3xl font-bold mr-4 text-gray-900'>
+                Bank Summary
+            </h2>
+            <div className='flex flex-col justify-center items-center bg-white rounded-md shadow-lg py-2 px-6'>
+                <p className='text-lg font-semibold text-gray-700'>
+                    Total Accounts:
+                    <span className='text-gray-600 ml-2 font-normal'>
+                        {accounts.length}
+                    </span>
+                </p>
+                <p className='text-lg font-semibold text-gray-700'>
+                    Total Money:
+                    <span className='text-gray-600 ml-2 font-normal'>
+                        ${totalMoney.toFixed(2)}
+                    </span>
+                </p>
+            </div>
         </div>
     );
 };
