8988f72d501f5bba0146ad032b6ab2ecd34f618c
Tautvydas Žukauskas <118177903+Zukauskas@users.noreply.github.com>
2023-03-03 01:28:57 +0200

    Form responsive styling

--- src/Components/AddNewAccount.jsx
+++ src/Components/AddNewAccount.jsx
@@ -22,24 +22,27 @@ const AddNewAccount = ({ addAccount }) => {
     return (
         <form
             onSubmit={dataHandler}
-            className='flex justify-center w-2/4 items-center flex-col gap-4 lg:flex-row bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4'
+            className='flex justify-center items-center flex-col gap-4 w-full sm:w-10/12 md:w-8/12 lg:w-7/12 2xl:w-6/12 lg:flex-row bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4'
         >
-            <label htmlFor='name' className='mr-2'>
+            <label htmlFor='name' className='mr-2 self-start lg:self-center'>
                 Name
             </label>
             <input
-                className='border border-gray-400 rounded py-2 px-4'
+                className='border border-gray-400 rounded py-2 px-4 w-full'
                 type='text'
                 id='name'
                 value={name}
                 onChange={nameHandler}
                 required
             />
-            <label htmlFor='lastName' className='mx-2'>
+            <label
+                htmlFor='lastName'
+                className='mx-2 self-start lg:self-center whitespace-nowrap'
+            >
                 Last Name
             </label>
             <input
-                className='border border-gray-400 rounded py-2 px-4'
+                className='border border-gray-400 rounded py-2 px-4 w-full'
                 type='text'
                 id='lastName'
                 value={lastName}
@@ -47,7 +50,7 @@ const AddNewAccount = ({ addAccount }) => {
                 required
             />
             <button
-                className='bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded ml-4'
+                className='bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 w-full rounded whitespace-nowrap'
                 type='submit'
             >
                 Add Account
