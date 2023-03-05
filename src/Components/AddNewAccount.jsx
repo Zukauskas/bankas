55dcd92e67f1348188ec8a40c84a73fe4ff2bd2f
Tautvydas Žukauskas <118177903+Zukauskas@users.noreply.github.com>
2023-03-05 23:19:42 +0200

    Use useReducer instead of useState

--- src/Components/AddNewAccount.jsx
+++ src/Components/AddNewAccount.jsx
@@ -1,28 +1,73 @@
-import { useState } from 'react';
+import { useReducer } from 'react';
+
+const initialState = {
+    firstName: '',
+    lastName: '',
+    errors: {
+        firstName: '',
+        lastName: '',
+    },
+};
+
+function reducer(state, action) {
+    switch (action.type) {
+        case 'setFirstName':
+            return {
+                ...state,
+                firstName: action.payload,
+                errors: {
+                    ...state.errors,
+                    firstName:
+                        action.payload.trim().length < 3 ||
+                        /\d/.test(action.payload)
+                            ? 'Name is not valid'
+                            : '',
+                },
+            };
+
+        case 'setLastName':
+            return {
+                ...state,
+                lastName: action.payload,
+                errors: {
+                    ...state.errors,
+                    lastName:
+                        action.payload.trim().length < 3 ||
+                        /\d/.test(action.payload)
+                            ? 'Last name is not valid'
+                            : '',
+                },
+            };
+
+        default:
+            return state;
+    }
+}
 
 const AddNewAccount = ({ addAccount }) => {
-    const [name, setName] = useState('');
-    const [lastName, setLastName] = useState('');
+    const [state, dispatch] = useReducer(reducer, initialState);
 
-    const nameHandler = (event) => {
-        setName(event.target.value);
-    };
+    function handleNameChange(event) {
+        dispatch({ type: 'setFirstName', payload: event.target.value });
+    }
 
-    const lastNameHandler = (event) => {
-        setLastName(event.target.value);
-    };
+    function handleLastNameChange(event) {
+        dispatch({ type: 'setLastName', payload: event.target.value });
+    }
 
     const dataHandler = (event) => {
         event.preventDefault();
-        addAccount(name, lastName);
-        setName('');
-        setLastName('');
+        if (!state.errors.firstName && !state.errors.lastName) {
+            addAccount(state.firstName, state.lastName);
+            state.firstName = '';
+            state.lastName = '';
+        }
     };
 
     return (
         <form
             onSubmit={dataHandler}
-            className='flex justify-center items-center flex-col gap-4 w-full sm:w-10/12 md:w-8/12 lg:w-7/12 2xl:w-6/12 lg:flex-row bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4'
+            className='flex justify-center items-center flex-col gap-4 w-full sm:w-10/12 md:w-8/12 lg:w-7/12 2xl:w-6/12 lg:flex-row bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 relative'
         >
             <label htmlFor='name' className='mr-2 self-start lg:self-center'>
                 Name
@@ -31,10 +76,15 @@ const AddNewAccount = ({ addAccount }) => {
                 className='border border-gray-400 rounded py-2 px-4 w-full'
                 type='text'
                 id='name'
-                value={name}
-                onChange={nameHandler}
+                value={state.firstName}
+                onChange={handleNameChange}
                 required
             />
+            {state.errors.firstName && (
+                <span className='absolute text-red-500 left-0 bottom-0'>
+                    {state.errors.firstName}
+                </span>
+            )}
             <label
                 htmlFor='lastName'
                 className='mx-2 self-start lg:self-center whitespace-nowrap'
@@ -45,10 +95,13 @@ const AddNewAccount = ({ addAccount }) => {
                 className='border border-gray-400 rounded py-2 px-4 w-full'
                 type='text'
                 id='lastName'
-                value={lastName}
-                onChange={lastNameHandler}
+                value={state.lastName}
+                onChange={handleLastNameChange}
                 required
             />
+            {state.errors.lastName && (
+                <span style={{ color: 'red' }}>{state.errors.lastName}</span>
+            )}
             <button
                 className='bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 w-full rounded whitespace-nowrap'
                 type='submit'
