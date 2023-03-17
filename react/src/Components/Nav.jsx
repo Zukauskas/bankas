96d0c86b2bd9eaebc8e1df7e353ad1ec0ee942b0
Tautvydas Žukauskas <118177903+Zukauskas@users.noreply.github.com>
2023-03-18 01:50:19 +0200

    Cleanup Nav component

--- react/src/Components/Nav.jsx
+++ react/src/Components/Nav.jsx
@@ -69,48 +69,6 @@ function Nav() {
       </div>
     </nav>
   );
-
-  {
-    /* <nav className=''>
-      <div className=''>Racoon bank</div>
-      <span
-        onClick={() => setRoute('home')}
-        className={route === 'home' ? 'active' : ''}>
-        Home
-      </span>
-      {authName ? (
-        <span
-          onClick={() => setRoute('accounts')}
-          className={'nav-link ' + (route === 'accounts' ? 'active' : '')}>
-          Accounts
-        </span>
-      ) : null}
-      <ul className='navbar-nav'>
-        {authName ? (
-          <>
-            <li className='nav-item'>
-              <span className='nav-link'>
-                <b>{authName}</b>
-              </span>
-            </li>
-            <li className='nav-item'>
-              <span className='nav-link' onClick={logOut}>
-                Logout
-              </span>
-            </li>
-          </>
-        ) : (
-          <li className='nav-item'>
-            <span
-              onClick={() => setRoute('login')}
-              className={'nav-link ' + (route === 'login' ? 'active' : '')}>
-              Login
-            </span>
-          </li>
-        )}
-      </ul>
-    </nav> */
-  }
 }
 
 export default Nav;
