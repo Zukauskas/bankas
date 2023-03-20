003cc952cc8ae407c152e6e42affde17da053d6f
Tautvydas Žukauskas <118177903+Zukauskas@users.noreply.github.com>
2023-03-20 13:47:52 +0200

    Make year dynamic

--- react/src/Components/Homepage.jsx
+++ react/src/Components/Homepage.jsx
@@ -4,6 +4,10 @@ import { Global } from './Global';
 const Homepage = () => {
   const { accounts } = useContext(Global);
 
+  const bankFounded = 2022;
+  const year = new Date().getFullYear();
+  const yearsInBusiness = year - bankFounded;
+
   const totalMoney = accounts
     ? accounts.reduce((acc, cur) => acc + cur.sum, 0)
     : 0;
@@ -24,30 +28,30 @@ const Homepage = () => {
                 ? (totalMoney / 1000).toFixed(0) + 'K'
                 : totalMoney < 999999999
                 ? (totalMoney / 1000000).toFixed(1) + 'M'
+                : totalMoney < 999999999999
+                ? (totalMoney / 1000000000).toFixed(1) + 'B'
                 : totalMoney.toFixed(2)}
             </h3>
           </div>
           <div className='w-32 h-32 border-2 border-red-500 bg-white/50 rounded-full shadow-lg flex justify-center items-center'>
-            <h3 className='text-2xl font-bold mb-2'>1</h3>
+            <h3 className='text-2xl font-bold mb-2'>{yearsInBusiness}</h3>
           </div>
         </div>
         <div className='flex justify-center items-center space-x-6 mt-2'>
           <div className='w-32 text-center'>
-            <p className='text-white text-lg'>Total Accounts</p>
+            <p className='text-white text-xl'>Total Accounts</p>
           </div>
           <div className='w-32 text-center'>
-            <p className='text-white text-lg'>Total Money</p>
+            <p className='text-white text-xl'>Total Money</p>
           </div>
           <div className='w-32 text-center'>
-            <p className='text-white text-lg'>Years in Business</p>
+            <p className='text-white text-xl'>Years in Business</p>
           </div>
         </div>
       </div>
       <div className='min-h-screen bg-cover bg-center bg-[url(./bg-racoon.jpg)] flex items-center'>
         <div className=' justify-self-start max-w-2xl ml-4 text-center text-black bg-white/75'>
-          <h1 className='text-5xl font-bold mb-4'>
-            Welcome to the Racoon Bank
-          </h1>
+          <h1 className='text-5xl font-bold m-4'>Welcome to the Racoon Bank</h1>
           <p className='text-2xl m-10'>
             We may not have the biggest stash, but we promise the best banking
             experience! Trust us, we won't play dead when it comes to your
