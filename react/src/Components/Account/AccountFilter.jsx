325c19bdc5676c51318d5517e6636a5e5a17b2da
Tautvydas Žukauskas <118177903+Zukauskas@users.noreply.github.com>
2023-03-24 13:56:32 +0200

    Update Filter component

--- react/src/Components/Account/AccountFilter.jsx
+++ react/src/Components/Account/AccountFilter.jsx
@@ -1,23 +1,22 @@
 const AccountFilter = ({ filterHandler }) => {
-    return (
-        <div className='flex items-center gap-2'>
-            <label
-                htmlFor='account-select'
-                className='block font-bold text-gray-700'
-            >
-                Accounts:
-            </label>
-            <select
-                name='account'
-                onChange={filterHandler}
-                className='border border-gray-400 rounded py-2 px-4'
-            >
-                <option value='All'>All</option>
-                <option value='withMoney'>With Money</option>
-                <option value='noMoney'>No Money</option>
-            </select>
-        </div>
-    );
+  return (
+    <div className='flex items-center gap-2'>
+      <label htmlFor='account-select' className='block font-bold text-gray-700'>
+        Accounts:
+      </label>
+      <select
+        name='account'
+        onChange={filterHandler}
+        className='border border-gray-400 rounded py-2 px-4'>
+        <option value='All'>All</option>
+        <option value='withMoney'>With Money</option>
+        <option value='withDebt'>With Debt</option>
+        <option value='noMoney'>No Money</option>
+        <option value='blocked'>Blocked</option>
+        <option value='active'>Active</option>
+      </select>
+    </div>
+  );
 };
 
 export default AccountFilter;
