9156e2e0f35dc1d3b756009cf9d69077eed6142c
Tautvydas Žukauskas <118177903+Zukauskas@users.noreply.github.com>
2023-03-24 10:45:16 +0200

    deposit confirm modal

--- react/src/Components/Account/AccountList.jsx
+++ react/src/Components/Account/AccountList.jsx
@@ -1,9 +1,13 @@
-import { useState, useContext } from 'react';
+import { useState, useContext, useEffect } from 'react';
 import AccountFilter from './AccountFilter';
 import { Global } from '../Global';
 
 const AccountList = () => {
   const [accountFilter, setAccountFilter] = useState('All');
+  const [depositConfirmModal, setDepositConfirmModal] = useState({
+    state: 'hidden',
+    approved: null,
+  });
 
   const [enteredAmount, setEnteredAmount] = useState({
     id: null,
@@ -17,6 +21,43 @@ const AccountList = () => {
     setDeletedAccount,
     setSumChanged,
   } = useContext(Global);
+
+  useEffect(() => {
+    if (depositConfirmModal.approved) {
+      const account = accounts.filter(acc => +acc.id === +enteredAmount.id)[0];
+      setSumChanged({
+        ...account,
+        sum: +(account.sum + +enteredAmount.amount).toFixed(2),
+      });
+
+      setEnteredAmount({
+        id: null,
+        amount: '',
+      });
+      setDepositConfirmModal({
+        state: 'hidden',
+        approved: null,
+      });
+    }
+  }, [depositConfirmModal]);
+
+  const approveDeposit = () => {
+    setDepositConfirmModal({
+      state: 'hidden',
+      approved: true,
+    });
+  };
+
+  const cancelDeposit = () => {
+    setDepositConfirmModal({
+      state: 'hidden',
+      approved: false,
+    });
+    setEnteredAmount({
+      id: null,
+      amount: '',
+    });
+  };
 
   const deleteHandler = id => {
     const account = [...accounts].filter(acc => acc.id === id);
@@ -61,15 +102,22 @@ const AccountList = () => {
     if (+enteredAmount.id === +id) {
       const account = accounts.filter(acc => +acc.id === +id)[0];
 
-      setSumChanged({
-        ...account,
-        sum: +(account.sum + +enteredAmount.amount).toFixed(2),
-      });
+      if (+enteredAmount.amount <= 1000) {
+        setSumChanged({
+          ...account,
+          sum: +(account.sum + +enteredAmount.amount).toFixed(2),
+        });
 
-      setEnteredAmount({
-        id: null,
-        amount: '',
-      });
+        setEnteredAmount({
+          id: null,
+          amount: '',
+        });
+      } else {
+        setDepositConfirmModal({
+          state: 'visible',
+          approved: null,
+        });
+      }
     }
   };
 
@@ -109,8 +157,6 @@ const AccountList = () => {
     setAccountFilter(e.target.value);
   };
 
-  console.log(accounts);
-
   const filteredAccounts = accounts
     ? accounts.filter(acc =>
         accountFilter === 'withMoney'
@@ -126,6 +172,50 @@ const AccountList = () => {
   return (
     <>
       <AccountFilter filterHandler={filterHandler} />
+      {/* Confirmation modal */}
+
+      <div
+        id='popup-modal'
+        tabindex='-1'
+        class={`${depositConfirmModal.state} fixed top-0 left-0 z-50 p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] md:h-full`}>
+        <div class='relative w-full h-full max-w-md md:h-auto'>
+          <div class='relative bg-white rounded-lg shadow dark:bg-gray-700'>
+            <div class='p-6 text-center'>
+              <svg
+                aria-hidden='true'
+                class='mx-auto mb-4 text-gray-400 w-14 h-14 dark:text-gray-200'
+                fill='none'
+                stroke='currentColor'
+                viewBox='0 0 24 24'
+                xmlns='http://www.w3.org/2000/svg'>
+                <path
+                  stroke-linecap='round'
+                  stroke-linejoin='round'
+                  stroke-width='2'
+                  d='M12 8v4m0 4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z'></path>
+              </svg>
+              <h3 class='mb-5 text-lg font-normal text-gray-500 dark:text-gray-400'>
+                Are you sure you want to deposit {enteredAmount.amount} to
+                account?
+              </h3>
+              <button
+                data-modal-hide='popup-modal'
+                type='button'
+                class='text-white bg-green-600 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center mr-2'
+                onClick={approveDeposit}>
+                Yes, I'm sure
+              </button>
+              <button
+                data-modal-hide='popup-modal'
+                type='button'
+                class='text-red-500 bg-white hover:bg-red-100 focus:ring-4 focus:outline-none focus:ring-red-200 rounded-lg border border-red-200 text-sm font-medium px-5 py-2.5 hover:text-red-900 focus:z-10 dark:bg-red-700 dark:text-red-300 dark:border-red-500 dark:hover:text-white dark:hover:bg-red-600 dark:focus:ring-red-600'
+                onClick={cancelDeposit}>
+                No, cancel
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
       <div
         className={`${showModal.state} ${showModal.color} w-1/3 px-2 py-4 fixed top-1 text-center rounded-md`}>
         <p>{showModal.message}</p>
