a6ef2acf296cb539c0a7cd764f7725c3fc38bf8f
Tautvydas Žukauskas <118177903+Zukauskas@users.noreply.github.com>
2023-03-27 03:08:39 +0300

    Homepage redesign

--- react/src/Components/Homepage.jsx
+++ react/src/Components/Homepage.jsx
@@ -13,61 +13,68 @@ const Homepage = () => {
     : 0;
 
   return (
-    <div className='bg-gray-100'>
-      <div className='absolute left-2/3 top-1/2 transform -translate-x-1/2'>
-        <div className='flex justify-center items-center  space-x-6'>
-          <div className='w-28 h-28 border-2 border-yellow-500 bg-white/50 rounded-full shadow-lg flex justify-center items-center'>
-            <h3 className='text-xl font-bold mb-2'>
-              {accounts ? accounts.length : 0}
-            </h3>
-          </div>
-          <div className='w-28 h-28 border-2 border-green-500  bg-white/50 rounded-full shadow-lg flex justify-center items-center'>
-            <h3 className='text-xl font-bold mb-2'>
-              €
-              {totalMoney < 999
-                ? totalMoney.toFixed(2)
-                : totalMoney < 999999
-                ? (totalMoney / 1000).toFixed(0) + 'K'
-                : totalMoney < 999999999
-                ? (totalMoney / 1000000).toFixed(1) + 'M'
-                : (totalMoney / 1000000000).toFixed(1) + 'B'}
-            </h3>
-          </div>
-          <div className='w-28 h-28 border-2 border-red-500 bg-white/50 rounded-full shadow-lg flex justify-center items-center'>
-            <h3 className='text-xl font-bold mb-2'>{yearsInBusiness}</h3>
-          </div>
-          <div className='w-28 h-28 border-2 border-blue-500  bg-white/50 rounded-full shadow-lg flex justify-center items-center'>
-            <h3 className='text-xl font-bold mb-2'>
-              €{(totalMoney / accounts.length).toFixed(2)}
-            </h3>
-          </div>
-        </div>
+    <div class='container my-24 px-6 mx-auto'>
+      <section class='mb-32 text-gray-800 text-center lg:text-left'>
+        <div class='block rounded-lg shadow-lg bg-white'>
+          <div class='flex flex-wrap items-center'>
+            <div class='block w-full lg:flex grow-0 shrink-0 basis-auto lg:w-6/12 xl:w-4/12'>
+              <img
+                src='./bg-racoon.jpg'
+                alt='Funny racoon'
+                class='w-full rounded-t-lg lg:rounded-tr-none lg:rounded-bl-lg'
+              />
+            </div>
+            <div class='grow-0 shrink-0 basis-auto w-full lg:w-6/12 xl:w-8/12'>
+              <div class='px-6 py-12 md:px-12'>
+                <h2 class='text-3xl font-bold mb-4 text-blue-600 display-5'>
+                  Welcome to the Racoon Bank
+                </h2>
+                <p class='text-gray-500 mb-12'>
+                  We may not have the biggest stash, but we promise the best
+                  banking experience! Trust us, we won't play dead when it comes
+                  to your money.
+                </p>
 
-        <div className='flex justify-center items-center space-x-10 mt-2'>
-          <div className='w-24 text-center'>
-            <p className='text-yellow-400 text-lg'>Total Accounts</p>
-          </div>
-          <div className='w-24 text-center'>
-            <p className='text-green-400 text-lg'>Total Money</p>
-          </div>
-          <div className='w-24 text-center'>
-            <p className='text-red-400 text-lg'>Years in Business</p>
-          </div>
-          <div className='w-24 text-center'>
-            <p className='text-blue-700 text-lg'>Average money in account</p>
+                <div class='grid lg:gap-x-12 md:grid-cols-3'>
+                  <div class='mb-12 md:mb-0'>
+                    <h2 class='text-3xl font-bold text-blue-600 mb-4'>
+                      {accounts ? accounts.length : 0}
+                    </h2>
+                    <h5 class='text-lg font-medium text-gray-500 mb-0'>
+                      Happy customers
+                    </h5>
+                  </div>
+
+                  <div class='mb-12 md:mb-0'>
+                    <h2 class='text-3xl font-bold text-blue-600 mb-4'>
+                      {totalMoney < 999
+                        ? totalMoney.toFixed(2)
+                        : totalMoney < 999999
+                        ? (totalMoney / 1000).toFixed(0) + 'K'
+                        : totalMoney < 999999999
+                        ? (totalMoney / 1000000).toFixed(1) + 'M'
+                        : (totalMoney / 1000000000).toFixed(1) + 'B'}
+                    </h2>
+                    <h5 class='text-lg font-medium text-gray-500 mb-0'>
+                      Total Money
+                    </h5>
+                  </div>
+
+                  <div class=''>
+                    <h2 class='text-3xl font-bold text-blue-600 mb-4'>
+                      {' '}
+                      €{(totalMoney / accounts.length).toFixed(2)}
+                    </h2>
+                    <h5 class='text-lg font-medium text-gray-500 mb-0'>
+                      Average account balance
+                    </h5>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
-      </div>
-      <div className='min-h-screen bg-cover bg-center bg-[url(./bg-racoon.jpg)] flex items-center'>
-        <div className=' justify-self-start max-w-2xl ml-4 text-center text-black bg-white/75'>
-          <h1 className='text-5xl font-bold m-4'>Welcome to the Racoon Bank</h1>
-          <p className='text-2xl m-10'>
-            We may not have the biggest stash, but we promise the best banking
-            experience! Trust us, we won't play dead when it comes to your
-            money.
-          </p>
-        </div>
-      </div>
+      </section>
     </div>
   );
 };
